f158a3a119f7abca45e1a6f9057ebbb969d96a75
Jorge Silveira <jlrs19833@gmail.com>
2021-07-30 11:15:38 -0300

    Criando nossa primeira aplicação - R Read

--- routes/person.js
+++ routes/person.js
@@ -1,8 +1,42 @@
 // const Express = require('express');
 const express = require("express");
+const { response } = require("../app");
 const router = express.Router();
 const Person = require("../models/Person");
 
-router.get("/", (request, response) => response.send("Hello"));
+// Find all
+router.get("/", (request, response) => {
+  Person.find({}, (err, people) => {
+    if (err) {
+      return;
+    }
+    response.send(people);
+  });
+});
+
+// Find one
+router.get("/:id", (request, response) => {
+  Person.findById(request.params.id, (err, person) => {
+    if (err) {
+      return;
+    }
+
+    response.send(person);
+  });
+
+  // Modo 2
+  /* Person.findOne(
+    {
+      _id: request.params.id,
+    },
+    (err, person) => {
+      if (err) {
+        return;
+      }
+
+      response.send(person);
+    }
+  ); */
+});
 
 module.exports = router;
