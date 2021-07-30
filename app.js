adb5d374cac93a5415d9f53ca24dc7bf1fc300ae
Jorge Silveira <jlrs19833@gmail.com>
2021-07-30 10:32:21 -0300

    Principais funções do Mongoose

--- app.js
+++ app.js
@@ -1,68 +1,127 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-var hbs = require('hbs');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var hbs = require("hbs");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // Helpers hbs
-hbs.registerHelper('date', () => {return new Date();});
-
-// Mongoose connect
-mongoose.connect('mongodb://localhost:27017/library', {useNewUrlParser: true, useUnifiedTopology: true});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));//() => console.log(`Ops! Something went wrong, mongoDB is broken...`))
-db.once('open', () => console.log(`Everything is okay, mongoDB is connected...`));
-
-var company = mongoose.Schema({
-  name: String
+hbs.registerHelper("date", () => {
+  return new Date();
 });
 
-var Company = mongoose.model('Company', company);
+// Mongoose connect
+mongoose.connect("mongodb://localhost:27017/library", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:")); //() => console.log(`Ops! Something went wrong, mongoDB is broken...`))
+db.once("open", () =>
+  console.log(`Everything is okay, mongoDB is connected...`)
+);
 
-/* Company.create({
-  name: 'Company 1',
-}, (err, company) => {
-  if (err) {
-    console.log('error');
-    return
+var company = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  address: {
+    name: String,
+    number: Number,
+    city: String,
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+var person = mongoose.Schema({
+  name: {
+    firstname: String,
+    lastname: String,
+  },
+});
+
+person.virtual("name.fullName").get(function () {
+  return this.name.firstname.concat(" ").concat(this.name.lastname);
+});
+
+var Person = mongoose.model("Person", person);
+
+Person.create(
+  {
+    name: {
+      firstname: "Jorge",
+      lastname: "Silveira",
+    },
+  },
+  (err, person) => {
+    if (err) {
+      console.log("Error Person ->", err);
+      return;
+    }
+
+    console.log(`Person Data -> ${person}`);
+    console.log(`Person Fullname -> ${person.name.fullName}`);
   }
-  console.log('Created ->', company);
-}); */
+);
+
+var Company = mongoose.model("Company", company);
+
+Company.create(
+  {
+    name: "Company 1",
+    address: {
+      name: "Address 1",
+      number: 35,
+      city: "Rio de Janeiro",
+    },
+  },
+  (err, company) => {
+    if (err) {
+      console.log("Error ->", err);
+      return;
+    }
+    console.log("Created ->", company);
+  }
+);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
